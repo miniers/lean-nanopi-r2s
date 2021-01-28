9d4687acf919c7e2811935111f0b0878b2d8805b
Miniers <m@lk.mk>
2021-01-28 08:30:57 +0800

    fix actions

--- build.sh
+++ build.sh
@@ -18,11 +18,11 @@ git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git l
 # luci-app-filebrowser
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-filebrowser lean/luci-app-filebrowser
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/filebrowser lean/filebrowser
-# luci-app-oled
-git clone --depth 1 https://github.com/NateLol/luci-app-oled.git lean/luci-app-oled
 
 # openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash lean/luci-app-openclash
+
+svn co https://github.com/songchenwen/nanopi-r2s/trunk/luci-app-r2sflasher lean/luci-app-r2sflasher
 
 #npc
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/npc lean/npc
